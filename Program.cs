0459e9874dc9b73a405a241f6a389fac5284380e
bzofkie <122408688+bzofkie@users.noreply.github.com>
2023-01-24 23:16:28 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Author: Barbara Zofkie
+ Date: 1/24/2023
+ Description: C# Console application for calculating letter grade
+ */
+
+using System;
+
+namespace Deliverable_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a numeric grade: ");
+
+            decimal grade = decimal.Parse(Console.ReadLine());
+
+            string letter;
+
+            try
+            {
+                if (grade >= 90)
+                {
+                    letter = "A";
+                    Console.WriteLine("Your expected letter grade for ISM 4300 is: " + letter);
+                }
+
+                else if (grade >= 80 && grade < 90)
+                {
+                    letter = "B";
+
+                }
+
+                else if (grade >= 70 && grade < 80)
+                {
+                    letter = "C";
+
+                }
+
+                else if (grade >= 60 && grade < 70)
+                {
+                    letter = "D";
+
+                }
+
+                else if (grade >= 00 && grade < 60)
+                {
+                    letter = "F";
+
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Enter a numeric grade: ");
+            }
+
+        }
+    }
+}
